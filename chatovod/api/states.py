24edad04113767aeb6c9dc18013caa58d695948e
Coquetoon <coque@ketche.net>
2018-01-04 01:26:02 -0200

    Add default values to state enums
    Some states are not sent by the server, thus the client must
    handle and assume they're the default value

--- chatovod/api/states.py
+++ chatovod/api/states.py
@@ -12,12 +12,15 @@ class AccountService(Enum):
     mail_ru = 'ma'
     ok_ru = 'od'
 
+    def _default(self):
+        return AccountService.guest
+
 
 class Language(Enum):
     """The language used in the interface of the chat service.
     It also determines the language of the messages returned by the API.
     i.e.: error messages and ban list text."""
-    default = 'None'
+    default = None
     azeri = 'az'
     dutch = 'nl'
     english = 'en'
@@ -26,12 +29,18 @@ class Language(Enum):
     tatar = 'tt'
     turkish = 'tr'
 
+    def _default(self):
+        return Language.default
+
 
 class RoomType(Enum):
     """The type of the room."""
     public = 0
     private = 1
     private_mask = 2
+
+    def _default(self):
+        return RoomType.public
 
 
 class Gender(Enum):
@@ -40,12 +49,18 @@ class Gender(Enum):
     female = 1
     male = 2
 
+    def _default(self):
+        return Gender.none
+
 
 class Group(Enum):
     """The group which the user belongs to."""
     user = 'user'
     moderator = 'moderator'
     admin = 'admin'
+
+    def _default(self):
+        return Group.user
 
 
 class Status(Enum):
@@ -55,3 +70,6 @@ class Status(Enum):
     # dnd stands for "do not disturb"
     busy = 'dnd'
     invisible = 'invis'
+
+    def _default(self):
+        return Status.online
