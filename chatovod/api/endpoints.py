1fa4df62144d232114773e2fa414ab7c4b018e2a
Coquetoon <coque@ketche.net>
2018-01-03 23:42:18 -0200

    Rename and sort endpoints accordingly

--- chatovod/api/endpoints.py
+++ chatovod/api/endpoints.py
@@ -25,32 +25,35 @@ class AccountEndpoint:
 
 
 class APIEndpoint:
-    """Endpoints for the Chatovod API."""
+    """Endpoints for the Chatovod chat API."""
     host = 'chatovod.com'
 
     # Chat
     CHAT_BIND = Route(Method.GET, '/chat/bind')
     CHAT_INFO_FETCH = Route(Method.GET, '/chat/start')
     CHAT_SESSION_FETCH = Route(Method.HEAD, '/')
-    CHAT_ROOMS_FETCH = Route(Method.GET, '/chat/load/rooms')
-    CHAT_ROOM_OPEN = Route(Method.POST, '/chat/openRoom')
-    CHAT_ROOM_CLOSE = Route(Method.POST, '/chat/closeRoom')
-    CHAT_PRIVATE_OPEN = Route(Method.POST, '/chat/openPrivate2')
     CHAT_BANS_FETCH = Route(Method.GET, '/chat/load/banlist')
-    CHAT_NICKNAME_BAN = Route(Method.POST, '/chat/ban')
-    CHAT_NICKNAME_UNBAN = Route(Method.POST, '/chat/unban')
+    CHAT_ROOMS_FETCH = Route(Method.GET, '/chat/load/rooms')
+
+    # Chat - Nickname
+    CHAT_NICKNAME_BAN      = Route(Method.POST, '/chat/ban')
+    CHAT_NICKNAME_UNBAN    = Route(Method.POST, '/chat/unban')
     CHAT_NICKNAME_MODERATE = Route(Method.GET, '/chat/getChatNickLocalModInfo')
-    CHAT_NICKNAME_FETCH = Route(Method.POST, '/chat/getChatNickLocal')
+    CHAT_NICKNAME_FETCH    = Route(Method.POST, '/chat/getChatNickLocal')
 
     # Room
-    ROOM_MESSAGE_SEND = Route(Method.POST, '/chat/send')
-    ROOM_MESSAGES_READ = Route(Method.POST, '/chat/markChannelRead')
+    ROOM_OPEN  = Route(Method.POST, '/chat/openRoom')
+    ROOM_PRIVATE_OPEN = Route(Method.POST, '/chat/openPrivate2')
+    ROOM_CLOSE = Route(Method.POST, '/chat/closeRoom')
+
+    ROOM_MESSAGE_SEND    = Route(Method.POST, '/chat/send')
+    ROOM_MESSAGES_READ   = Route(Method.POST, '/chat/markChannelRead')
     ROOM_MESSAGES_DELETE = Route(Method.POST, '/chat/deleteMessages')
-    ROOM_MESSAGES_FETCH = Route(Method.GET, '/chat/loadLastMessages')
+    ROOM_MESSAGES_FETCH  = Route(Method.GET, '/chat/loadLastMessages')
 
     # User
-    USER_CHAT_JOIN = Route(Method.POST, '/chat/auth')
+    USER_CHAT_ENTER  = Route(Method.POST, '/chat/auth')
     USER_CHAT_LEAVE = Route(Method.GET, '/chat/signOut')
-    USER_AGE_SET = Route(Method.POST, '/chat/setAge')
+    USER_AGE_SET    = Route(Method.POST, '/chat/setAge')
     USER_STATUS_SET = Route(Method.POST, '/chat/setStatus')
-    USER_REGISTER = Route(Method.GET, '/register')
+    USER_REGISTER   = Route(Method.GET, '/register')
