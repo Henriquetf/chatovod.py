d47d896fafd5bea1ed574ff911629781f31fbd35
Coquetoon <coque@ketche.net>
2018-01-24 23:12:21 -0200

    Make a draft for Message
    Based on the old library

--- chatovod/structures/message.py
+++ chatovod/structures/message.py
@@ -1,26 +1,27 @@
 
-class MessageBase:
 
-    def __init__(self, *, event):
-        self.id = str(event['ts'])
 
-        self._old = event.get('s', False)
-        self.fetched = event.get('pp', False)
+class Message:
+
+    def __init__(self, *, room, data):
+        self.id = data['timestamp']
+
+        self.content = data['content']
+        self.to = data.get('to', [])
+        self.cmd_me = data.get('nh', False)
+
+        self.seen = not data.get('u', True)
+
+        self._old = data.get('s', False)
+        self.fetched = data.get('pp', False)
+        self.room = room
+        self._handle_author(data)
+
+    def _handle_author(self, data):
+        self.author = author
+
 
     @property
     def old(self):
         return self._old or self.fetched
 
-
-class Message(MessageBase):
-
-    def __init__(self, *, author, room, event):
-        self.author = author
-        self.room = room
-
-    def _update(self, event):
-        self.content = event['m']
-        self.to = event.get('to', [])
-        self.cmd_me = event.get('nh', False)
-
-        self.seen = not event.get('u', True)
