4be45cda61d726230f2bbcccc52a38ffa6e07614
Coquetoon <coque@ketche.net>
2018-01-24 23:04:30 -0200

    Rename InternalState to Chat, add basic core

---
+++ chatovod/api/chat.py
@@ -0,0 +1,66 @@
+from .http import HTTPClient
+from collections import deque, OrderedDict
+
+
+class Chat:
+
+    def __init__(self, client, http):
+        self.client = client
+        self.http = http
+
+    def reset(self):
+        self._users = OrderedDict()
+        self._rooms = OrderedDict()
+        self._emojis = {}
+        self._emojis_groups = []
+        self._window_id = 0
+
+    def handle_start(self, data):
+        start_data = StartData(data)
+
+        for user_data in start_data['users']:
+            user = User(data=user_data, state=self)
+
+        for room_data in start_data['rooms']:
+            room = Room(data=room_data, state=self)
+            self._add_room(room)
+
+        emojis = start_data['emojis']
+        emoji_groups = start_data['emoji_groups']
+
+        client_info = start_data['client_info']
+        self._window_id = client_info['window_id']
+
+    def handle_user_enter_chat(self, event):
+        user = User(data=event, state=self)
+        self._add_user(user)
+
+        self.dispatch('user_enter_chat', user)
+
+    def handle_user_leave_chat(self, event):
+        user = self.get_user(event['nickname'])
+        self._remove_user(user)
+
+        self.dispatch('user_leave_chat', user)
+
+    def handle_user_enter_room(self, event):
+        pass
+
+    def _get_user(self, nickname):
+        return self.users[nickname.lower()]
+
+    def _add_user(self, user):
+        nickname = user.nickname.lower()
+        self._users[nickname] = user
+
+    def _remove_user(self, user):
+        return self._users.pop(user.nickname.lower())
+
+    def _get_room(self, room_id):
+        return self._rooms.get(room_id)
+
+    def _add_room(self, room):
+        self._rooms[room.id] = room
+
+    def _remove_room(self, room):
+        self._rooms.pop(room.id)
--- chatovod/api/internal.py
+++
@@ -1,8 +0,0 @@
-from chatovod.api.http import HTTPClient
-
-
-class InternalState:
-
-    def __init__(self, *, client):
-        self.client = client
-        self.http = HTTPClient()
