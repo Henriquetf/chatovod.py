792fd2ae93ce3d3b84c1b2313ec6ee3c59700c93
Coquetoon <coque@ketche.net>
2018-02-01 16:58:05 -0200

    Add errors and error_factory

--- chatovod/core/errors.py
+++ chatovod/core/errors.py
@@ -1,2 +1,33 @@
-class ChatovodError(Exception):
+from chatovod.api.event_adapter import adapt_error
+
+
+class ChatovodException(Exception):
     """Base exception for errors caused by the library."""
+
+
+class HTTPException(ChatovodException):
+    """ """
+
+class ConnectionError(HTTPException):
+    """ """
+
+class ConnectionReset(ConnectionError):
+    """ """
+
+class Forbidden(HTTPException):
+    """ """
+
+
+def error_factory(raw):
+    error = adapt_error(raw)
+    error_type = error['type']
+
+    if error_type == 'connection':
+        if error['group'] == 'reset':
+            return ConnectionReset
+        else:
+            return ConnectionError
+    elif error_type == 'auth':
+        return Forbidden
+    else:
+        return HTTPException
