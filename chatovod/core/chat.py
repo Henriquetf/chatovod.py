5b92409cda7a2ad2df7d2c5c31a5a88574ab4d37
Coquetoon <coque@ketche.net>
2018-02-01 11:49:47 -0200

    Rewrite transform_start into handle_start, assign values in set_option

--- chatovod/core/chat.py
+++ chatovod/core/chat.py
@@ -2,47 +2,6 @@ from .http import HTTPClient
 from collections import deque, OrderedDict
 
 
-def _make_room_patch(raw):
-    return {
-        'room': raw,
-        'messages': [],
-        'errors': [],
-        'has_older_events': None
-    }
-
-def transform_start(data):
-    patch = {
-        'options': [],
-        'chat_emojis': [],
-        'rooms': {},
-    }
-
-    for _raw in data:
-        transformed, raw = transform_event(_raw)
-        event = raw.get('t')
-
-        if 'room_id' in raw:
-            room_id = raw['room_id']
-            room = patch['rooms'].get(room_id)
-
-        if event == 'set_option':
-            patch['options'].append(raw)
-        elif event == 'chat_emojis':
-            patch['chat_emojis'].append(raw)
-        elif event == 'room_open':
-            patch['rooms'][room_id] = _make_room_patch(raw)
-        elif event == 'message':
-            room['messages'].append(raw)
-        elif event == 'error':
-            room['errors'].append(raw)
-        elif event == 'has_older_events':
-            room['has_older_events'] = raw['value']
-        else:
-            print('Unhandled event "{}", transformed: {}'.format(_raw, transformed))
-
-    return patch
-
-
 class ChatState:
 
     def __init__(self, client, http):
@@ -62,30 +21,40 @@ class ChatState:
         return self._http.url
 
     def handle_start(self, data):
-        self.handle_chat_emojis(data['chat_emojis'])
+        for _raw in data:
+            transformed, raw = transform_event(_raw)
+            event = raw.get('t')
 
-        for option in data['options']:
-            self.handle_set_option(option)
+            if event in ('set_option', 'chat_emojis', 'error'):
+                handler = getattr(self, 'handle_' + event, None)
 
-        for room_info in data['rooms']:
-            room_data = room_info['room']
-            room = self._create_room(room_data)
-            self._add_room(room)
+                handler(raw, start=True)
+            elif event == 'room_open':
+                room = self._create_room(raw)
+                self._add_room(room)
+            elif event == 'message':
+                message = self._create_message(message)
+                self._cache_message(message)
+            elif event == 'has_older_events':
+                # TODO: Implement this
+                ...
+            else:
+                log.info('Unhandled event "{}", transformed: {}'.format(_raw, transformed))
 
-    def handle_set_option(self, raw):
+    def handle_set_option(self, raw, start=False):
         option = raw['option']
         value = raw.get('value')
 
         if option == 'nick':
-            set_nickname(value)
-        elif option == 'wid':
-            set_window_id(value)
+            self._user.nickname = value
         elif option == 'signedIn':
-            set_signedIn(value)
+            self._user.signed_in = value
+        elif option == 'wid':
+            self._http.window_id = value
         else:
             log.info('Unhandled option {} {}'.format(option, raw))
 
-    def handle_chat_emojis(self, raw):
+    def handle_chat_emojis(self, raw, start=False):
         self._emojis_base_path = raw['default_path']
         self._custom_emojis_base_path = raw.get('custom_path')
 
@@ -96,6 +65,9 @@ class ChatState:
         for raw_emoji in raw['emojis']:
             emoji = self._create_emoji(raw_emoji)
             self._add_emoji(emoji)
+
+    def handle_error(self, raw, start=False):
+        ...
 
     def handle_room_open(self, event):
         pass
