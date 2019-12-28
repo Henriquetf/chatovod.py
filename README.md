a1062fbd5450b362f5abe5b04ab1eff82524c167
Henrique Torres <henriquetfw@gmail.com>
2019-12-28 17:17:06 -0300

    Setup tests and packaging configuration with Cookiecutter

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# chatovod.py
+===========
+chatovod.py
+===========
 
-An open source rewrite of the old library used for NotReallyABot.
+.. image:: https://img.shields.io/pypi/v/chatovod_py.svg
+:target: https://pypi.python.org/pypi/chatovod_py
+
+.. image:: https://img.shields.io/travis/Henriquetf/chatovod_py.svg
+:target: https://travis-ci.org/Henriquetf/chatovod_py
+
+.. image:: https://readthedocs.org/projects/chatovod-py/badge/?version=latest
+:target: https://chatovod-py.readthedocs.io/en/latest/?badge=latest
+:alt: Documentation Status
+
+Chatovod Python library for bot lovers.
+
+-   Free software: Apache Software License 2.0
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from chatovod import __version__
+
+from setuptools import find_packages, setup
+
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup_requirements = ["pytest-runner"]
+
+test_requirements = ["pytest>=3"]
+
+setup(
+    author="Henrique Torres",
+    python_requires=">=3.5",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="Chatovod Python library for bot lovers.",
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    long_description=readme,
+    include_package_data=True,
+    keywords="chatovod.py",
+    name="chatovod.py",
+    packages=find_packages(include=["chatovod.py", "chatovod.py.*"]),
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/Henriquetf/chatovod.py",
+    version=__version__,
+    zip_safe=False,
+)
